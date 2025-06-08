d0da5e7a61754d64e77aeeb8f35c5fad56d2cd29
Moro <124579207+MohammadEbrahimMorovati@users.noreply.github.com>
2025-06-08 15:52:51 +0330

    Update menu.py

--- robot-tel/handlers/menu.py
+++ robot-tel/handlers/menu.py
@@ -1,19 +1,21 @@
 # handlers/menu.py
+
 from telebot import types
 
 def register_handlers(bot):
-    @bot.message_handler(commands=['menu'])
+    @bot.message_handler(commands=['start', 'menu'])  # هر دو دستور فعال
     def show_menu(message):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 
         markup.add(
-            types.KeyboardButton("🔍 جستجو"),
-            types.KeyboardButton("⚡ جستجوی پیشرفته"),
-            types.KeyboardButton("🎬 تازه‌ها"),
-            types.KeyboardButton("🇮🇷 ایرانی"),
-            types.KeyboardButton("📺 یوتوب"),
-            types.KeyboardButton("💠 دسته‌بندی"),
-            types.KeyboardButton("🎬 برترین‌ها")
+            types.KeyboardButton("جستجو پیشرفته ⚡"), types.KeyboardButton("جستجو 🔍"),
+            types.KeyboardButton("یوتوب 🔴"), types.KeyboardButton("ایرانی 🇮🇷"),
+            types.KeyboardButton("زیرنویس 🔍"), types.KeyboardButton("تازه‌ها 🔥"),
+            types.KeyboardButton("پروفایل من 🧑🏻‍💼"), types.KeyboardButton("پلن‌ها 🎭"),
+            types.KeyboardButton("سینما نیوز 🗞️"), types.KeyboardButton("تاریخچه 🧾"),
+            types.KeyboardButton("جدول پخش 🗓️"), types.KeyboardButton("دسته‌بندی 💠"),
+            types.KeyboardButton("برترین‌ها 🎬"), types.KeyboardButton("پرترین‌ها 📩"),
+            types.KeyboardButton("پیشنهاد‌ی 🎲"), types.KeyboardButton("بیشتر ➕")
         )
 
-        bot.send_message(message.chat.id, "📋 منوی اصلی:", reply_markup=markup)
+        bot.send_message(message.chat.id, "📋 منوی اصلی ربات:", reply_markup=markup)
